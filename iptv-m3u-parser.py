e57014056c2c3d101ac98d25a07add9b28b4fc43
alxdc <alxdc@protonmail.com>
2018-04-14 14:15:18 +0200

    First template

---
+++ iptv-m3u-parser.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+class Parser(object):
+
+    def __init__(self):
+        pass
+        
+    def _getUserDetails(self):
+        self.username = 
+        self.password = 
+        
+    def getUserGroups(self, username):
+        
+        # Read user wanted groups from file
+        readFile()
+        
+        return groupList
+        
+    def waitForConnection(self):
+        self.url = 
+        self._getUserDetails()
+        
+    def getm3u(self):
+        pass
+        
+    def sendm3u(self):
+        pass
+		
+    def getAvailableGroups(self):
+        pass
+        
+     
+if __name__ == '__main__':
+
+    # Object init
+    parser = Parser()
+
+    # Wait for incoming connection
+    parser.waitForConnection()
