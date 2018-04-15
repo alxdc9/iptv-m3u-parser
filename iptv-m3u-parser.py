430db2f039b75654ba230770ac2255c48e182ee7
alxdc <alxdc@protonmail.com>
2018-04-15 14:34:40 +0200

    Working server and parameters parser

--- iptv-m3u-parser.py
+++ iptv-m3u-parser.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python3
+import http.server
+import socketserver
+from urllib.parse import urlparse, parse_qs
+import os
+import shutil
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+
+#     def __init__(self):
+#         pass
+    FILEPATH = 'sample.txt'
+
+    def do_GET(self):
+        # Construct a server response.
+        self.parsedURL = parse_qs(urlparse(self.path).query)
+        
+        self.getParameters()
+        
+        print(self.groups)
+        
+        with open(self.FILEPATH, 'rb') as f:
+            self.send_response(200)
+            self.send_header("Content-Type", 'application/octet-stream')
+            self.send_header("Content-Disposition", 'attachment; filename="{}"'.format(os.path.basename(self.FILEPATH)))
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs.st_size))
+            self.end_headers()
+            shutil.copyfileobj(f, self.wfile)
+          
+    def getParameters(self):
+        """
+        Gets username and password
+        """
+        self.username = self.parsedURL['user'][0]
+        self.password = self.parsedURL['pass'][0]
+        self.groups = self.parsedURL['groups'][0].split(',')
 
 
-class Parser(object):
-
-    def __init__(self):
-        pass
-        
-    def _getUserDetails(self):
-        self.username = 
-        self.password = 
-        
-    def getUserGroups(self, username):
-        
-        # Read user wanted groups from file
-        readFile()
-        
-        return groupList
-        
-    def waitForConnection(self):
-        self.url = 
-        self._getUserDetails()
-        
-    def getm3u(self):
-        pass
-        
-    def sendm3u(self):
-        pass
-		
-    def getAvailableGroups(self):
-        pass
-        
-     
 if __name__ == '__main__':
 
-    # Object init
-    parser = Parser()
-
-    # Wait for incoming connection
-    parser.waitForConnection()
+    print('Server listening on port 8000...')
+    httpd = socketserver.TCPServer(('', 8000), Handler)
+    httpd.serve_forever()
